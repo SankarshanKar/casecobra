987f6fbfa1257f7e0f575b7afdb117cceaaa76aa
Sankarshan Kar <sankarshan.kar.sk@gmail.com>
2024-05-14 01:25:44 +0530

    Added database functionality and started Configurator

---
+++ src/app/configure/design/DesignConfigurator.tsx
@@ -0,0 +1,16 @@
+interface DesignConfiguratorProps {
+  configId: string;
+  imageUrl: string;
+  imageDimensions: {
+    width: number;
+    height: number;
+  };
+}
+
+const DesignConfigurator = ({
+  configId,
+  imageUrl,
+  imageDimensions,
+}: DesignConfiguratorProps) => {};
+
+export default DesignConfigurator;
--- src/app/configure/design/page.tsx
+++ src/app/configure/design/page.tsx
@@ -1,3 +1,30 @@
-const Page = () => {};
+import { db } from "@/db";
+import { notFound } from "next/navigation";
+
+interface PageProps {
+  searchParams: {
+    [key: string]: string | string[] | undefined;
+  };
+}
+
+const Page = async ({ searchParams }: PageProps) => {
+  const { id } = searchParams;
+
+  if (!id || typeof id !== "string") {
+    return notFound();
+  }
+
+  const configuration = await db.configuration.findUnique({
+    where: { id },
+  });
+
+  if (!configuration) {
+    return notFound();
+  }
+
+  const { imageUrl, width, hieght } = configuration;
+
+  return <div></div>;
+};
 
 export default Page;
